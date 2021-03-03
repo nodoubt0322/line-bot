157b9d184453066ba5c0a749ad9f106173c592d9
Daniel.h <daniel.h@oaoooa.com>
2021-03-03 17:52:28 +0800

    to string

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: 1655718134,
+  channelId: "1655718134",
   channelSecret: "481b0e9de7669b34cd70d321f85cedbf",
-  channelAccessToken: "x4b/37bMUW0JwD8WrGR4u0cCaNI2dmAVXhJMAcYS6ZKnpTNPShbDq9q4OIpR+usVt0gh9XQyCjdv0N1VSU9HfbnfrWfe30fng4jKQ5TKElugD4VDAYX+YbgX6FGOy5G+2IX3QGAqjoSpAW2kIJDzKQdB04t89/1O/w1cDnyilFU="
+  channelAccessToken: "x4b/37bMUW0JwD8WrGR4u0cCaNI2dmAVXhJMAcYS6ZKnpTNPShbDq9q4OIpR+usVt0gh9XQyCjdv0N1VSU9HfbnfrWfe30fng4jKQ5TKElugD4VDAYX+YbgX6FGOy5G+2IX3QGAqjoSpAW2kIJDzKQdB04t89/1O/w1cDnyilFU=",
 });
 
 bot.on('message', function(event) {
